a1553ffad6705dfa56eb477ed4c3a1856a38c8ce
Nils Ivanson <nils@ivanson.se>
2015-06-27 01:18:16 +0200

    do not put event and other props on load since it might be loaded from other places than components

--- src/actions/infoActions.js
+++ src/actions/infoActions.js
@@ -4,7 +4,7 @@ import {
         INFO_LOAD_FAIL
         } from './actionTypes';
 
-export function load(event, {props, state}, updateState) {
+export function load() {
     return {
         types: [INFO_LOAD, INFO_LOAD_SUCCESS, INFO_LOAD_FAIL],
         promise: (client) => loadFromServer(client)
